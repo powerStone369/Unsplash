7d214fc81092b5ce3ddc2206b4c424fd384d2419
johyeon yoon <jalynneyoon@gmail.com>
2021-12-12 14:41:45 +0900

    Initial commit

---
+++ Unsplash/UnsplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashApp.swift
+//  Unsplash
+//
+//  Created by Johyeon Yoon on 2021/12/12.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
